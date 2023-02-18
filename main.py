de7700f99bb376498ba67384a2544c76c9dca36a
ChiradeepNanabala <44319198+ChiradeepNanabala@users.noreply.github.com>
2023-02-17 19:55:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import time
 from plyer import notification
 
 while True:
+    # Send the Notification
     notification.notify(
         app_icon = 'water-glass-color-icon.ico',
         title = "Reminder :)",
         message = "Stay Hydrated by Drinking Water !!",
         timeout = 15
     )
+    # Remind every hour
     time.sleep(3600)
